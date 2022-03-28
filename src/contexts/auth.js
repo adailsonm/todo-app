abbf674384bf43ffab258f45c8aa8b291d1f994b
Adailson Moreira <adailson.moreira@pontaltech.com.br>
2022-03-27 23:41:21 -0300

    refreshToken expired token

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,8 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { api } from "../service/api";
+import decode from 'jwt-decode';
+
 
 const AuthContext = createContext({});
 
@@ -8,15 +10,25 @@ export const AuthProvider = ({children}) => {
     const [token, setToken] = useState(null);
     const [name, setName] = useState(null);
     const [isAuthenticate, setIsAuthenticate] = useState(false);
+    const [expiredToken, setExpiredToken] = useState(false);
+    const [payload, setPayload] = useState({});
+    const [refreshToken, setRefreshToken] = useState('');
 
     useEffect(() => {
       const storagedToken = localStorage.getItem("@App:token");
+      const storagedRefreshToken = localStorage.getItem("@App:refreshToken");
       const storagedName = localStorage.getItem("@App:name");
       if(storagedToken) {
         setIsAuthenticate(true);
         setToken(storagedToken);
         setName(storagedName);
-        
+        setRefreshToken(storagedRefreshToken);
+        const { exp, email, _id, name} = decode(storagedToken);
+        if (exp <= Math.floor(new Date() / 1000)) {
+          setExpiredToken(true);
+          setPayload({email, _id, name});
+        }
+
         api.defaults.headers.Authorization = `Bearer ${storagedToken}`;
       }
     }, []);
@@ -26,8 +38,10 @@ export const AuthProvider = ({children}) => {
         const response = await api.post('/users/login', data);
         api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
         localStorage.setItem('@App:token', response.data.token);
+        localStorage.setItem("@App:refreshToken", response.data.refreshToken);
         localStorage.setItem('@App:name', response.data.name);
         setToken(response.data.token);
+        setRefreshToken(response.data.refreshToken);
         setName(response.data.name);
         setIsAuthenticate(true);
       } catch(error) {
@@ -48,10 +62,31 @@ export const AuthProvider = ({children}) => {
     function Logout() {
       localStorage.removeItem('@App:token');
       localStorage.removeItem('@App:name');
+      localStorage.removeItem('@App:refreshToken');
+
       setToken(null);
       setIsAuthenticate(false)
+      setRefreshToken(null);
       setName('');
     }
+
+    async function handleRefreshToken() {
+      console.log(payload)
+      let response = await api.post('/users/refresh',{
+        payload,
+        refreshToken
+      });
+
+      api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+      localStorage.setItem('@App:token', response.data.token);
+      localStorage.setItem('@App:name', response.data.name);
+    }
+
+    if (expiredToken) {
+      handleRefreshToken();
+      setExpiredToken(false);
+    }
+  
 
     return (
       <AuthContext.Provider value={{ isAuthenticate, name, token, Login, Logout}}>
